e1af539d5b7741a423a4e3b6b3745885c5856f7a
Jasur <jasurmutalov2001014@gmail.com>
2022-05-20 14:10:08 +0500

    secuirty

--- src/main/java/uz/unicorn/rentme/dto/advertisement/AdvertisementUpdateDTO.java
+++ src/main/java/uz/unicorn/rentme/dto/advertisement/AdvertisementUpdateDTO.java
@@ -2,7 +2,13 @@ package uz.unicorn.rentme.dto.advertisement;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.geo.Point;
 import uz.unicorn.rentme.dto.base.GenericDTO;
+import uz.unicorn.rentme.dto.transport.TransportCreateDTO;
+import uz.unicorn.rentme.enums.AdvertisementCategory;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -10,7 +16,24 @@ import uz.unicorn.rentme.dto.base.GenericDTO;
 @AllArgsConstructor
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@ParameterObject
 public class AdvertisementUpdateDTO extends GenericDTO {
+
     private String title;
+
     private String description;
+
+    private Long price;
+
+    private AdvertisementCategory category;
+
+    private Point location;
+
+    private LocalDateTime startDate;
+
+    private Long minDuration;
+
+    private Long maxDuration;
+
+    private TransportCreateDTO transport;
 }
