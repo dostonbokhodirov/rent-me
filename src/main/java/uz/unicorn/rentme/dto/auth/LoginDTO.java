482dde8756a2aa24b84233ab37e9afa1c949cf48
Doston Bokhodirov <76911351+dostonbokhodirov@users.noreply.github.com>
2022-05-24 16:11:10 +0500

    updated AdvertisementRepository

--- src/main/java/uz/unicorn/rentme/dto/auth/LoginDTO.java
+++ src/main/java/uz/unicorn/rentme/dto/auth/LoginDTO.java
@@ -3,7 +3,6 @@ package uz.unicorn.rentme.dto.auth;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springdoc.api.annotations.ParameterObject;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -12,10 +11,10 @@ import javax.validation.constraints.Pattern;
 @Setter
 @NoArgsConstructor
 public class LoginDTO {
-    @NotBlank
+    @NotBlank(message = "Phone number cannot be blank")
     @Pattern(regexp = "^\\+\\d{12}(\\d{2})?$", message = "Phone number is invalid")
     private String phoneNumber;
 
-    @NotBlank
+    @NotBlank(message = "Phone number cannot be blank")
     private String code;
 }
